fa1292cb7b10704cbfe68605d3ab0f988c08872f
Maciej Górski <maciek.gorski@gmail.com>
2015-05-09 16:22:16 +0200

    Show near places with beer.

--- app/src/main/java/pl/beerlurk/beerlurk/MapActivity.java
+++ app/src/main/java/pl/beerlurk/beerlurk/MapActivity.java
@@ -1,7 +1,9 @@
 package pl.beerlurk.beerlurk;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +12,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import pl.beerlurk.beerlurk.dto.BeerLocationsWrapper;
+import pl.beerlurk.beerlurk.dto.DistancedBeerLocation;
+import pl.beerlurk.beerlurk.dto.Factory;
+import pl.beerlurk.beerlurk.service.BeerApi;
+import pl.beerlurk.beerlurk.service.BeerService;
+import pl.beerlurk.beerlurk.service.GeocodeApi;
+import pl.beerlurk.beerlurk.service.MatrixApi;
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 public class MapActivity extends Activity implements OnMapReadyCallback {
 
@@ -51,5 +71,45 @@ public class MapActivity extends Activity implements OnMapReadyCallback {
                 .title("Sydney")
                 .snippet("The most populous city in Australia.")
                 .position(sydney));
+
+        doCall(map);
+    }
+
+    private void doCall(final GoogleMap map) {
+        Location myLocation = new Location("my");
+        myLocation.setLatitude(52.2237465);
+        myLocation.setLongitude(20.9476116);
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+        RestAdapter adapter = new RestAdapter.Builder()
+                .setEndpoint("https://maps.googleapis.com")
+                .setConverter(new GsonConverter(gson))
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .build();
+        MatrixApi matrixApi = adapter.create(MatrixApi.class);
+        GeocodeApi geocodeApi = adapter.create(GeocodeApi.class);
+        new BeerService(new BeerApi() {
+            @Override
+            public Observable<BeerLocationsWrapper> call() {
+                return Observable.just(Factory.create());
+            }
+        }, matrixApi, geocodeApi).call("beer", myLocation)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<DistancedBeerLocation>>() {
+                    @Override
+                    public void call(List<DistancedBeerLocation> distancedBeerLocations) {
+                        Log.e("tag", "count: " + distancedBeerLocations.size());
+                        Log.e("tag", distancedBeerLocations + "");
+                        for (DistancedBeerLocation l : distancedBeerLocations) {
+                            map.addMarker(new MarkerOptions().position(new LatLng(l.getLocation().getLatitude(), l.getLocation().getLongitude())));
+                        }
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        Log.e("tag", "error", throwable);
+                    }
+                });
     }
 }
